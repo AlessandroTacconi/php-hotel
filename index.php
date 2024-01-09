a5b4d8ec2aed6543293ad1307fca9245f2261762
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2024-01-09 16:25:02 +0100

    feat: tab

--- index.php
+++ index.php
@@ -46,29 +46,44 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <title>PHP Hotel</title>
 </head>
 <body>
-  <hr>
-  <?php
-    foreach ($hotels as $hotel) {
-      echo $hotel['name'];
-      echo '<br>';
-      echo $hotel['description'];
-      echo '<br>';    
-      if ($hotel['parking'] === true) {
-        echo 'Parcheggio';
-        } else {
-        echo 'No Parcheggio';
-      }
-      echo '<br>';
-      echo $hotel['vote'];
-      echo '<br>';
-      echo $hotel['distance_to_center'];
-      echo '<br>';
-      echo '<hr>';
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nome Hotel</th>
+      <th scope="col">Descrizione Hotel</th>
+      <th scope="col">Parcheggio</th>
+      <th scope="col">Voto</th>
+      <th scope="col">Distanza dal centro</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+      foreach ($hotels as $hotel) {
+      echo '<tr>';
+        echo '<td scope="row">' . $hotel['name'] . '</td>';
+        echo '<td>' . $hotel['description'] . '</td>';
+        echo '<td>';
+        if ($hotel['parking'] === true) {
+            echo 'Parcheggio';
+            } else {
+            echo 'No Parcheggio';
+          };
+          
+        echo '</td>';
+        echo '<td>' . $hotel['vote'] . '/5' . '</td>';
+        echo '<td>' . $hotel['distance_to_center'] . ' km' . '</td>';
+      echo '</tr>';
+      
+
     }
   ?>
+  </tbody>
+</table>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
 
